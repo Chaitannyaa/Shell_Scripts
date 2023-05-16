f40d2410037ce5a876cf0601055a73794c91d22f
root <root@ip-10-0-1-6.ec2.internal>
2023-05-16 14:03:26 +0000

    Add jenkins installation script

---
+++ install-docker-jenkins-RHEL.sh
@@ -0,0 +1,32 @@
+# SHELL SCRIPT TO INSTALL DOCKER AND JENKINS
+
+# Install Docker
+sudo yum update
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+# Install Jenkins
+# Jenkins requires Java to run, so first install Java
+sudo yum update
+sudo yum install java-11-openjdk -y
+
+# Long-Term Support release of Jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk -y 
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Verify installations
+docker --version
+java -version
+jenkins --version
+sudo systemctl status jenkins
---
+++ install-docker-jenkins-debian.sh
@@ -0,0 +1,28 @@
+#Install Docker
+
+sudo apt-get update
+
+sudo apt-get install docker.io -y
+
+sudo usermod -aG docker $USER
+
+#Install Jenkins
+#Jenkins requires Java to run, so first install Java -->
+
+sudo apt-get update
+
+sudo apt install openjdk-11-jre -y
+
+#Long-Term Support release of Jenkins---->
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins -y
+
+docker --version
+java -version
+jenkins --version
