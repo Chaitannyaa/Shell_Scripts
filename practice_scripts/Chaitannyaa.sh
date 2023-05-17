c6717f1afc922dbe4bb93b790fbfce91d513eb11
Chaitannyaa Gaikwad <117350787+Chaitannyaa@users.noreply.github.com>
2023-05-17 23:25:33 +0530

    fix the bug of handling similar values from both players

--- practice_scripts/Chaitannyaa.sh
+++ practice_scripts/Chaitannyaa.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-echo "Let's find the greater number from your and computer choice"
+echo "Let's find the greater number between your choice and the computer's choice."
 
-echo "Please choose any number from 1-100"
-
+echo "Please choose any number from 1-100."
 read Player
+if ! [[ $Player =~ ^[1-9][0-9]?$|^100$ ]]; then
+    echo "Please enter a valid number from 1-100 only."
+    exit 1
+fi
 
 sleep 3
 
 Computer=$((1 + RANDOM % 100))
 
-echo "Computer choice is $Computer"
-
-echo "Your choice is $Player"
+echo "Computer choice is $Computer."
+echo "Your choice is $Player."
 
 sleep 3
 
-if [ $Computer -ge $Player ]
-then
-	echo "Computer Won the match"
+if [ $Computer -gt $Player ]; then
+    echo "Computer won the match."
+elif [ $Player -gt $Computer ]; then
+    echo "You won the match."
 else
-	echo "You Won the match"
+    echo "Your choice matched with the computer's choice."
+    echo "You both can get married :)"
 fi
